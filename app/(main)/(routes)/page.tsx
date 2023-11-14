c86afddd2f2549825cdff937c653a87684f22a56
Rohan Sawai <sawairohan90@gmail.com>
2023-11-14 19:27:35 +0530

    added authentication and dark theme

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
-import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/mode-toggle"
+import { UserButton } from "@clerk/nextjs"
 
 export default function Home() {
   return (
     <div>
-      This is a protected route!!!
+      <UserButton afterSignOutUrl="/"/>    
+      <ModeToggle/>
     </div>
   )
 }
