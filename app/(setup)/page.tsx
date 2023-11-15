45303d36e2d060a489bb468c34620ca22db22d50
Rohan Sawai <sawairohan90@gmail.com>
2023-11-15 18:20:32 +0530

    added prisma schema and createed user profile

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,25 @@
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile";
+import { redirect } from "next/navigation";
+const SetupPage = async () => {
+    const profile = await initialProfile();
+
+    const server = await db.server.findFirst({
+        where: {
+            member: {
+                some: {
+                    profileId: profile.id
+                }
+            }
+        }
+    })
+
+    if(server) {
+        return redirect('/server/${server.id}');   
+    }
+    return ( 
+        <div>Create a Server</div>
+     );
+}
+ 
+export default SetupPage;
