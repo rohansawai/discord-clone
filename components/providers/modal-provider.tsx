ae95db0ea6b7959c3c83c5a965c30428a0801237
Rohan Sawai <sawairohan90@gmail.com>
2023-11-25 02:47:34 +0530

    added all modals

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -1,9 +1,18 @@
 "use client";
 
-import { CreateServerModal } from "@/components/modals/create-server-modal";
-import { InviteModal } from "@/components/modals/invite-modal";
-import { EditServerModal } from "@/components/modals/edit-server-modal";
 import { useEffect, useState } from "react";
+
+import { EditServerModal } from "@/components/modals/edit-server-modal";
+import { InviteModal } from "@/components/modals/invite-modal";
+import { CreateServerModal } from "@/components/modals/create-server-modal";
+import { MembersModal } from "@/components/modals/members-modal";
+import { CreateChannelModal } from "@/components/modals/create-channel-modal";
+import { LeaveServerModal } from "@/components/modals/leave-server-modal";
+import { DeleteServerModal } from "@/components/modals/delete-server-modal";
+import { DeleteChannelModal } from "@/components/modals/delete-channel-modal";
+import { EditChannelModal } from "@/components/modals/edit-channel-modal";
+import { MessageFileModal } from "@/components/modals/message-file-modal";
+import { DeleteMessageModal } from "@/components/modals/delete-message-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -21,6 +30,14 @@ export const ModalProvider = () => {
       <CreateServerModal />
       <InviteModal />
       <EditServerModal />
+      <MembersModal />
+      <CreateChannelModal />
+      <LeaveServerModal />
+      <DeleteServerModal />
+      <DeleteChannelModal />
+      <EditChannelModal />
+      <MessageFileModal />
+      <DeleteMessageModal />
     </>
-  );
-};
+  )
+}
