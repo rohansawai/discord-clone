37b52bbd908757178bbf552cbb5447225ef5a920
Rohan Sawai <sawairohan90@gmail.com>
2023-12-09 15:04:45 +0530

    version update

--- next.config.js
+++ next.config.js
@@ -1,11 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    webpack: (config) => {
+      config.externals.push({
+        "utf-8-validate": "commonjs utf-8-validate",
+        bufferutil: "commonjs bufferutil"
+      });
+  
+      return config;
+    },
     images: {
-        domains: [
-            "uploadthing.com",
-            "utfs.io"
-        ]
+      domains: [
+        "uploadthing.com",
+        "utfs.io"
+      ]
     }
-}
-
-module.exports = nextConfig
+  }
+  
+  module.exports = nextConfig
