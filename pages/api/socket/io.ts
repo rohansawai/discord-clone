d50c2d3625435022c330ba601ad73af4bd525c65
Rohan Sawai <sawairohan90@gmail.com>
2023-12-09 15:55:20 +0530

    deployment optimization

--- pages/api/socket/io.ts
+++ pages/api/socket/io.ts
@@ -16,13 +16,11 @@ const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
     const httpServer: NetServer = res.socket.server as any;
     const io = new ServerIO(httpServer, {
       path: path,
-      // @ts-ignore
-      addTrailingSlash: false,
     });
     res.socket.server.io = io;
   }
 
   res.end();
-}
+};
 
 export default ioHandler;
